71af46acf577e2e20ff0accbb7d670b78b8e30c3
Roee Landesman <roee.landesman@gmail.com>
2018-04-21 14:15:11 -0700

    Changing main file

--- Lab3/main.s
+++ Lab3/main.s
@@ -12,8 +12,27 @@ main:
     @ driver function main lives here, modify this for your other functions
     @ You'll need to scan characters for the operation and to determine
     @ if the program should repeat.
-    @ To scan a character, and compare it to another, do the following
-      ldr     r0, =scanchar
+    @ To scan a character, and compare it to another, do the following  
+      push {r5, r6, lr}
+loop:
+      ldr  r0, =prompt1       @ Ask for user number
+      bl printf 
+
+      ldr r0, =scannumber
+      mov r1, sp
+      bl scanf
+      ldr r6, [sp]            @ Save the scanned value into r6
+
+      ldr r0, =prompt2
+      bl printf               @ Ask for user number (second number)
+
+      ldr r0, =scannumber
+      mov r1, sp
+      bl scanf
+      ldr r7, [sp]            @ Save the scanned value into r7 (second number)
+
+      
+
       mov     r1, sp          @ Save stack pointer to r1, you must create space
       bl      scanf           @ Scan user's answer
       ldr     r1, =yes        @ Put address of 'y' in r1
@@ -28,3 +47,13 @@ yes:
     .byte   'y'
 scanchar:
     .asciz  " %c"
+scannumber:
+    .asciz  " %d"
+printnum:
+   .asciz "\n%d"
+prompt1:
+  .asciz "Enter number 1 please: "
+prompt2:
+  .asciz "Enter number 2 please: "
+operation:
+  .asciz "Enter operation please: "
