bdc65e921fed801cae0f29a524408c39bf31985b
Roee Landesman <roee.landesman@gmail.com>
2018-04-30 13:20:12 -0700

    progress

--- lab4-matadd-master/matadd.s
+++ lab4-matadd-master/matadd.s
@@ -1,30 +1,14 @@
-    /* This function has 5 parameters, and the declaration in the
-       C-language would look like:
-
-       void matadd (int **C, int **A, int **B, int height, int width)
-
-       C, A, B, and height will be passed in r0-r3, respectively, and
-       width will be passed on the stack.
-
-       You need to write a function that computes the sum C = A + B.
-
-       A, B, and C are arrays of arrays (matrices), so for all elements,
-       C[i][j] = A[i][j] + B[i][j]
-
-       You should start with two for-loops that iterate over the height and
-       width of the matrices, load each element from A and B, compute the
-       sum, then store the result to the correct element of C. 
-
-       This function will be called multiple times by the driver, 
-       so don't modify matrices A or B in your implementation.
-
-       As usual, your function must obey correct ARM register usage
-       and calling conventions. */
-
-	.arch armv7-a
-	.text
-	.align	2
-	.global	matadd
-	.syntax unified
-	.arm
 matadd:
+   push {r4,r5,r6,r7,r8,r9,r10}
+   push {lr}
+   ldr   r4, [sp, #32]  @ r4: width
+   mov   r5, #0         @ r5: i = 0
+
+loop_i_height: 
+   cmp   r5, r3
+   beq   exit            @ if i >= height, end
+   mov   r6, #0         @ reset j = 0 each time outer loop is run
+
+exit:
+   pop {r4,r5,r6,r7,r8,r9,r10}
+   pop {lr}
