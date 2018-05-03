af2b012e682b150c585e8f7f527913a1fcbaa493
Roee Landesman <roee.landesman@gmail.com>
2018-05-02 23:32:43 -0700

    more

--- lab4-matadd-master/matadd.s
+++ lab4-matadd-master/matadd.s
@@ -29,42 +29,39 @@
    .arm
 
 @ Input parameters: r0 = C, r1 = A, r2 = B, r3 = height, and width is on stack
+@ r4 = i
+@ r5 = j
+@ Using two decreminting for loops to go through input arrays 
 matadd:
    push {r4, r5, r6, r7, r8, r9, r10, lr}
-   ldr r5, [sp, #32]                          @ j initialized to width
    mov r4, r3                                 @ i initialized to height
 
 height_loop_outer:
    cmp r4, #0                                 @ if(i == 0)
    beq exit                    
    sub r4, r4, #1                             @ decrement i
+   ldr r5, [sp, #32]                          @ reset j to width each time this loop is run
 
 width_loop_inner:
    cmp r5, #0                                  @ if(j==0)
    beq height_loop_outer                       
    sub r5, r5, #1                              @ decrement j
-   
-   @ load the address of A[i] into r8, with lsl used to move through matrix
-   @ then load A[i][j] into r8
-   ldr   r8, [r1, r5, lsl #2]    
-   ldr   r8, [r8, r6, lsl #2]    
-
-   @ same as above, but with B[i][j] into r9
-
-   ldr   r9, [r2, r5, lsl #2]   
-   ldr   r9, [r9, r6, lsl #2]    
-
-   @ add the two values of the matrices and put in r10
-   add   r10, r8, r9    
-
+   @ load the address of A[i] into r6, LSL used to multiply height and width by 2.
+   @ then load A[i][j] into r6 using address from above
+   ldr r6, [r1, r4, lsl #2]
+   ldr r6, [r6, r5, lsl #2]
+   @ same as above, but with B[i][j] into r7
+   ldr r7, [r2, r4, lsl #2]
+   ldr r7, [r7, r5, lsl #2]
+   @ add the two values of the matrices and put in r8
+   add r8, r6, r7
    @ load the address of C[i] into r7
-   ldr   r7, [r0, r5, lsl #2]   
-
-   @ store value of C[i][j] (calculated using r7 from above) into r10
-   str   r10, [r7, r6, lsl #2] 
+   ldr r9, [r0, r4, lsl #2]
+   @ store value of C[i][j] (calculated using r9 from above) into r8
+   str r8, [r9, r5, lsl #2]
    b width_loop_inner
 
-end:
+exit:
   pop {r4, r5, r6, r7, r8, r9, r10, pc}
 
 print:
