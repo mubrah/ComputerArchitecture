68ffb3ab8f42874af76db00d19f4bbed69c58ae4
Roee Landesman <roee.landesman@gmail.com>
2018-06-03 14:29:31 -0700

    ALU Stats updated

--- final_project/execute.cpp
+++ final_project/execute.cpp
@@ -262,54 +262,69 @@ void execute() {
           setCarryOverflow(rf[alu.instr.lsli.rm], alu.instr.lsli.imm, OF_SHIFT);
           setNegativeFlag(rf[alu.instr.lsli.rm] << alu.instr.lsli.imm);
           setZeroFlag(rf[alu.instr.lsli.rm] << alu.instr.lsli.imm);
+          stats.numRegWrites++;
+          stats.numRegReads++;
           break;
         case ALU_ADDR:
           rf.write(alu.instr.addr.rd, rf[alu.instr.addr.rn] + rf[alu.instr.addr.rm]);
           setCarryOverflow(rf[alu.instr.addr.rn],rf[alu.instr.addr.rm],OF_ADD);
           setNegativeFlag(rf[alu.instr.addr.rn] + rf[alu.instr.addr.rm]);
           setZeroFlag(rf[alu.instr.addr.rn] + rf[alu.instr.addr.rm]);
+          stats.numRegWrites++;
+          stats.numRegReads += 2;
           break;
         case ALU_SUBR:
           rf.write(alu.instr.subr.rd, rf[alu.instr.subr.rn] - rf[alu.instr.subr.rm]);
           setCarryOverflow(rf[alu.instr.subr.rn],rf[alu.instr.subr.rm],OF_SUB);
           setNegativeFlag(rf[alu.instr.subr.rn] - rf[alu.instr.subr.rm]);
           setZeroFlag(rf[alu.instr.subr.rn] - rf[alu.instr.subr.rm]);
+          stats.numRegWrites++;
+          stats.numRegReads += 2;
           break;
         case ALU_ADD3I:
           rf.write(alu.instr.add3i.rd, rf[alu.instr.add3i.rn] + alu.instr.add3i.imm);
           setCarryOverflow(rf[alu.instr.add3i.rn],alu.instr.add3i.imm,OF_ADD);
           setNegativeFlag(rf[alu.instr.add3i.rn] + alu.instr.add3i.imm);
+          stats.numRegWrites++;
+          stats.numRegReads++;
           break;
         case ALU_SUB3I:
           rf.write(alu.instr.sub3i.rd, rf[alu.instr.sub3i.rn] - alu.instr.sub3i.imm);
           setCarryOverflow(rf[alu.instr.sub3i.rn], alu.instr.sub3i.imm ,OF_SUB);
           setNegativeFlag(rf[alu.instr.sub3i.rn] - alu.instr.sub3i.imm);
           setZeroFlag(rf[alu.instr.sub3i.rn] - alu.instr.sub3i.imm);
+          stats.numRegWrites++;
+          stats.numRegReads++;
           break;
         case ALU_MOV:
           rf.write(alu.instr.mov.rdn, alu.instr.mov.imm);
           setNegativeFlag(alu.instr.mov.imm);
           flags.C = 0; //manual set of zero 
           setCarryOverflow(rf[alu.instr.mov.rdn],alu.instr.mov.imm, OF_SUB);
-          //stats.numRegWrites += 1;
+          stats.numRegWrites++;
           break;
         case ALU_CMP:
           // NO rf.write, will subtract cmp.rdn - cmp.imm and update flags accordingly 
           setCarryOverflow(rf[alu.instr.cmp.rdn], alu.instr.cmp.imm, OF_SUB);
           setNegativeFlag(rf[alu.instr.cmp.rdn] - alu.instr.cmp.imm);
           setZeroFlag(rf[alu.instr.cmp.rdn] - alu.instr.cmp.imm);
+          stats.numRegReads++;
           break;
         case ALU_ADD8I:
           rf.write(alu.instr.add8i.rdn, rf[alu.instr.add8i.rdn] + alu.instr.add8i.imm);
           setCarryOverflow(rf[alu.instr.add8i.rdn], alu.instr.add8i.imm, OF_ADD);
           setNegativeFlag(rf[alu.instr.add8i.rdn] + alu.instr.add8i.imm);
           setZeroFlag(rf[alu.instr.add8i.rdn] + alu.instr.add8i.imm);
+          stats.numRegWrites++;
+          stats.numRegReads++;
           break;
         case ALU_SUB8I:
           rf.write(alu.instr.sub8i.rdn, rf[alu.instr.sub8i.rdn] - alu.instr.sub8i.imm);
           setCarryOverflow(rf[alu.instr.sub8i.rdn], alu.instr.sub8i.imm, OF_SUB);
           setNegativeFlag(rf[alu.instr.sub8i.rdn] - alu.instr.sub8i.imm);
           setZeroFlag(rf[alu.instr.sub8i.rdn] - alu.instr.sub8i.imm);
+          stats.numRegWrites++;
+          stats.numRegReads++;
           break;
         default:
           cout << "instruction not implemented" << endl;
